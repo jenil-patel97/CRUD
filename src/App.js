183a573b1e1c241c2d7ea60e3eb0828bdac1a3db
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-10-28 16:52:33 +0530

    feat: Todo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./Todo";
 
 function App() {
+  const [todos, setTodos] = useState(["Learn react", "Grind next"]);
+
   return (
     <div className="h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]">
       <div className="bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4">
-        <h3 className="text-2xl font-bold text-center text-gray-800 p-2">
-          TODO App
+        <h3 className="text-2xl font-bold text-center text-gray-800 p-2 cursor-default">
+          Todo App
         </h3>
         <form className="flex justify-between">
           <input
@@ -20,8 +23,13 @@ function App() {
         </form>
 
         <ul>
-          
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
         </ul>
+        <p className="text-center p-2 underline cursor-default">
+          You Have 2 Todos
+        </p>
       </div>
     </div>
   );
